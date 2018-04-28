aa8cd06cfc78630bab4ed63a37820128eda24fa4
Hristo Gatsinski <gatsinski@gmail.com>
2018-04-28 11:31:18 +0300

    Change `CommonProvider`
    Add private variables
    Add `isAuthenticated` method
    Add `setAuthenticationData` method
    Add `performRequest` method
    Add `toCamelCase` method
    Add `getHeaders` method

--- src/providers/common/common.ts
+++ src/providers/common/common.ts
@@ -1,11 +1,71 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+import { ENV } from '@app/env';
 
 @Injectable()
 export class CommonProvider {
+  private _env = ENV;
+  private _accessToken: string;
+  private _refreshToken: string;
+  private _scope: string;
 
   constructor(public http: HttpClient) {
-    console.log('Hello CommonProvider Provider');
   }
 
+  get isAuthenticated() {
+     return this._accessToken ? true : false;
+  }
+
+  public setAuthenticationData(data) {
+    this._accessToken = data.access_token;
+    this._refreshToken = data.refresh_token;
+    this._scope = data.scope;
+  }
+
+  public performRequest(relativeUrl: string,
+    requestMethod: string,
+    requestData?: object | HttpParams) {
+
+    const url = `${this._env.apiUrl}/${relativeUrl}`;
+    const data = JSON.stringify(requestData);
+    const headers = this.getHeaders(requestMethod);
+    const options = {
+      headers: headers
+    };
+
+    switch (requestMethod) {
+      case 'PUT': {
+        return this.http.put(url, data, options);
+      }
+      case 'POST': {
+        return this.http.post(url, data, options);
+      }
+      default: {  // GET
+        const options = {
+          params: <HttpParams>requestData,
+          headers: headers
+        };
+        return this.http.get(url, options);
+      }
+    }
+  }
+
+  public toCamelCase(s) {
+    return s.replace(/([\-_]\w)/g, function(m) {
+      return m[1].toUpperCase();
+    });
+  }
+
+  private getHeaders(requestMethod: string) {
+    let headers: HttpHeaders = new HttpHeaders();
+
+    headers = headers.append('Content-Type', 'application/json');
+
+    if (this._accessToken) {
+      headers = headers.append('Authorization', `Bearer ${this._accessToken}`);
+    }
+
+    return headers;
+  }
 }
