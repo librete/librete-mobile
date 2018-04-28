654dbc63c87bd3b6ba3b544fb371c78aa7d1a142
Hristo Gatsinski <gatsinski@gmail.com>
2018-04-28 17:02:52 +0300

    Add form to `SignUpPage`

--- src/pages/sign-up/sign-up.ts
+++ src/pages/sign-up/sign-up.ts
@@ -1,17 +1,68 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NavController, NavParams } from 'ionic-angular';
+
+import { SignInPage } from '../sign-in/sign-in';
+
+import { CommonProvider } from './../../providers/common/common';
+import { AuthProvider } from './../../providers/auth/auth';
 
 @Component({
   selector: 'page-sign-up',
   templateUrl: 'sign-up.html',
 })
 export class SignUpPage {
+  public form: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    private formBuilder: FormBuilder,
+    private commonProvider: CommonProvider,
+    private authProvider: AuthProvider) {
+
+    this.form = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.minLength(4)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(4)]],
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+    });
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SignUpPage');
+  public get username() {
+    return this.form.get('username');
+  }
+
+  public get email() {
+    return this.form.get('email');
+  }
+
+  public get password() {
+    return this.form.get('password');
+  }
+
+  public get firstName() {
+    return this.form.get('firstName');
+  }
+
+  public get lastName() {
+    return this.form.get('lastName');
+  }
+
+  public signUp() {
+    this.authProvider.signUp(this.form.value).subscribe(
+      data => {
+        this.navCtrl.setRoot(SignInPage);
+      },
+      error => {
+        for (let key in error.error) {
+          key = this.commonProvider.toCamelCase(key);
+          if (this.form.get(key)) {
+            this.form.get(key).setErrors({remote: error.error[key]});
+          }
+        }
+      }
+    );
   }
 
 }
