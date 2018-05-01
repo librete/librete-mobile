f282b2215b7af977e74b71e2c7f086ba90fc7375
Hristo Gatsinski <gatsinski@gmail.com>
2018-05-01 18:59:08 +0300

    Fix `app.component.spec.ts`

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,34 @@
-import { async, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { TestBed, async } from '@angular/core/testing';
 import { IonicModule, Platform } from 'ionic-angular';
 
+import { IonicStorageModule } from '@ionic/storage';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { MyApp } from './app.component';
-import {
-  PlatformMock,
-  StatusBarMock,
-  SplashScreenMock
-} from '../../test-config/mocks-ionic';
+import { PlatformMock, SplashScreenMock, StatusBarMock } from '../../test-config/mocks-ionic';
+
+import { Observable } from 'rxjs/Observable';
+
+import { CommonProvider } from '../providers/common/common';
+import { AuthProvider } from '../providers/auth/auth';
+
+class CommonProviderStub {
+  isAuthenticated = true;
+  readAuthenticationData() {
+    return new Promise(resolve => { resolve(); });
+  }
+  setAuthenticationData() {
+    return;
+  }
+}
+
+class AuthProviderStub {
+  refreshToken(): Observable<Object> {
+    return Observable.of({'token': 'Token value'});
+  }
+}
 
 describe('MyApp Component', () => {
   let fixture;
@@ -19,14 +38,18 @@ describe('MyApp Component', () => {
     TestBed.configureTestingModule({
       declarations: [MyApp],
       imports: [
-        IonicModule.forRoot(MyApp)
+        IonicModule.forRoot(MyApp),
+        IonicStorageModule.forRoot(),
+        HttpClientModule,
       ],
       providers: [
         { provide: StatusBar, useClass: StatusBarMock },
         { provide: SplashScreen, useClass: SplashScreenMock },
-        { provide: Platform, useClass: PlatformMock }
+        { provide: Platform, useClass: PlatformMock },
+        { provide: CommonProvider, useClass: CommonProviderStub },
+        { provide: AuthProvider, useClass: AuthProviderStub }
       ]
-    })
+    });
   }));
 
   beforeEach(() => {
