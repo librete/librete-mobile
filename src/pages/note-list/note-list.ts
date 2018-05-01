25ffadfced90021a4eecc00109d727a13ea21df2
Hristo Gatsinski <gatsinski@gmail.com>
2018-05-01 16:47:31 +0300

    Show notes in `NoteListPage`

--- src/pages/note-list/note-list.ts
+++ src/pages/note-list/note-list.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { NotesProvider } from './../../providers/notes/notes';
+
 @Component({
   selector: 'page-note-list',
   templateUrl: 'note-list.html',
 })
 export class NoteListPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    private notesProvider: NotesProvider) {
+
+    notesProvider.updateNotes();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad NoteListPage');
+  get notes() {
+    return this.notesProvider.notes.getValue();
   }
-
 }
