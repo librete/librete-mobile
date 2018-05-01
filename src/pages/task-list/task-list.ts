bd1090d8d833c9312754a391e6a3b7ce36fc494a
Hristo Gatsinski <gatsinski@gmail.com>
2018-05-01 17:17:01 +0300

    Show tasks in `TaskListPage`

--- src/pages/task-list/task-list.ts
+++ src/pages/task-list/task-list.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { TasksProvider } from './../../providers/tasks/tasks';
+
 @Component({
   selector: 'page-task-list',
   templateUrl: 'task-list.html',
 })
 export class TaskListPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    private tasksProvider: TasksProvider) {
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad TaskListPage');
-  }
+      tasksProvider.updateTasks();
+    }
 
+    get tasks() {
+      return this.tasksProvider.tasks.getValue();
+    }
 }
