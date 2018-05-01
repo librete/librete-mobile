2646372ff885d98af74c073eeca7c5cf5602b13f
Hristo Gatsinski <gatsinski@gmail.com>
2018-05-01 13:14:02 +0300

    Add `Note` model

---
+++ src/models/note.ts
@@ -0,0 +1,25 @@
+import {JsonObject, JsonProperty} from 'json2typescript';
+
+@JsonObject
+export class Note {
+  @JsonProperty('url')
+  url: string = undefined;
+
+  @JsonProperty('name')
+  name: string = undefined;
+
+  @JsonProperty('author')
+  authorUrl: string = undefined;
+
+  @JsonProperty('category')
+  categoryUrl: string = undefined;
+
+  @JsonProperty('text')
+  text: string = undefined;
+
+  @JsonProperty('created_at')
+  createdAt: Date = undefined;
+
+  @JsonProperty('updated_at')
+  updatedAt: Date = undefined;
+}
