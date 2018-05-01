75325b8612d8eb5e10d71da23edb8ffdecd73d8b
Hristo Gatsinski <gatsinski@gmail.com>
2018-05-01 10:55:35 +0300

    Show events in `EventListPage`

--- src/pages/event-list/event-list.ts
+++ src/pages/event-list/event-list.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { EventsProvider } from './../../providers/events/events';
+
 @Component({
   selector: 'page-event-list',
   templateUrl: 'event-list.html',
 })
 export class EventListPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    private eventsProvider: EventsProvider) {
+
+    eventsProvider.updateEvents();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad EventListPage');
+  get events() {
+    return this.eventsProvider.events.getValue();
   }
-
 }
