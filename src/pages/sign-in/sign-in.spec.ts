e412bd418a122e8d5a3583cac51f50b112690ca5
Hristo Gatsinski <gatsinski@gmail.com>
2018-05-06 11:22:32 +0300

    Add `SignInPage` tests

---
+++ src/pages/sign-in/sign-in.spec.ts
@@ -0,0 +1,66 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
+import { NavController, NavParams } from 'ionic-angular';
+
+import { SignInPage } from './sign-in';
+import { CommonProvider } from './../../providers/common/common';
+import { AuthProvider } from './../../providers/auth/auth';
+
+class NavControllerStub {}
+class NavParamsStup {}
+class CommonProviderStub {}
+class AuthProviderStub {}
+
+let component: SignInPage;
+let fixture: ComponentFixture<SignInPage>;
+
+describe('Pages: SignInPage', () => {
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [SignInPage],
+      providers: [
+        FormBuilder,
+        { provide: NavController, useClass: NavControllerStub },
+        { provide: NavParams, useClass: NavParamsStup },
+        { provide: CommonProvider, useClass: CommonProviderStub },
+        { provide: AuthProvider, useClass: AuthProviderStub }
+      ],
+      schemas: [ NO_ERRORS_SCHEMA ]
+    });
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SignInPage);
+    component = fixture.componentInstance;
+  });
+
+  function updateForm(username, password) {
+    component.username.setValue(username);
+    component.password.setValue(password);
+  }
+
+  it('Should be valid', () => {
+    updateForm('test_username', 'test_password');
+    expect(component.form.valid).toBeTruthy();
+    expect(component.form.value).toEqual({
+      'username': 'test_username',
+      'password': 'test_password'
+    });
+  });
+
+  it('Should be invalid due to missing required fields', () => {
+    expect(component.form.valid).toBeFalsy();
+    expect(component.username.errors['required']).toBeTruthy();
+    expect(component.password.errors['required']).toBeTruthy();
+  });
+
+  it('Should be invalid due to short field values', () => {
+    updateForm('u', 'p');
+    expect(component.form.valid).toBeFalsy();
+    expect(component.username.errors['minlength']).toBeTruthy();
+    expect(component.password.errors['minlength']).toBeTruthy();
+  });
+
+});
