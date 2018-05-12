35ff7aa75015922815865ec9500c6523159add39
Hristo Gatsinski <gatsinski@gmail.com>
2018-05-12 16:59:44 +0300

    Add navigation from `TaskListPage` to `TaskDetailPage`

--- src/pages/task-list/task-list.ts
+++ src/pages/task-list/task-list.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
 import { TasksProvider } from './../../providers/tasks/tasks';
+import { TaskDetailPage } from '../task-detail/task-detail';
+import { Task } from '../../models/task';
 
 @Component({
   selector: 'page-task-list',
@@ -19,4 +21,8 @@ export class TaskListPage {
     get tasks() {
       return this.tasksProvider.tasks.getValue();
     }
+
+  public navigateToDetail(task: Task) {
+    this.navCtrl.push(TaskDetailPage, { task: task });
+  }
 }
