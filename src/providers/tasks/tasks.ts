028e65fed9081d05a42cc5678f240888266c7a63
Hristo Gatsinski <gatsinski@gmail.com>
2018-05-24 16:27:56 +0300

    Refactor `TasksProvider`

--- src/providers/tasks/tasks.ts
+++ src/providers/tasks/tasks.ts
@@ -3,9 +3,9 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { JsonConvert } from 'json2typescript';
 
-import { CommonProvider } from './../common/common';
-import { CategoriesProvider } from './../categories/categories';
-import { Task } from './../../models/task';
+import { CommonProvider } from '../common/common';
+import { CategoriesProvider } from '../categories/categories';
+import { Task } from '../../models/task';
 
 @Injectable()
 export class TasksProvider {
@@ -20,65 +20,23 @@ export class TasksProvider {
   ];
   private _tasks = new BehaviorSubject<Array<Task>>([]);
 
-  constructor(private commonProvider: CommonProvider,
-    private categoriesProvider: CategoriesProvider) {
-  }
+  constructor(
+    private _commonProvider: CommonProvider,
+    private _categoriesProvider: CategoriesProvider
+  ) {}
 
-  get tasks() {
+  public get tasks() {
     return this._tasks;
   }
 
-  public updateTask(url: string, data: any) {
+  public readTasks(): Promise<boolean> {
     return new Promise ((resolve, reject) => {
-      data.start_date = new Date(data.startDate);
-      data.end_date = new Date(data.endDate);
-      this.commonProvider.performRequest(url, 'PUT', data).subscribe(
-        (data: any) => {
-          const jsonConvert: JsonConvert = new JsonConvert();
-          const task: Task = jsonConvert.deserialize(data, Task);
-          const tasks: Array<Task> = this._tasks.getValue();
-          const index = tasks.findIndex(x => x.url === url);
-
-          this.setCategory(task);
-          tasks[index] = Object.assign(tasks[index], task);
-
-          resolve(task);
-        },
-        error => {
-          reject(error);
-        }
-      );
-    });
-  }
-
-  public createTask(data: any) {
-    return new Promise ((resolve, reject) => {
-      data.start_date = new Date(data.startDate);
-      data.end_date = new Date(data.endDate);
-      this.commonProvider.performRequest('tasks/', 'POST', data).subscribe(
-        (data: any) => {
-          const jsonConvert: JsonConvert = new JsonConvert();
-          const task: Task = jsonConvert.deserialize(data, Task);
-          const tasks: Array<Task> = this._tasks.getValue();
-          this.setCategory(task);
-          tasks.push(task);
-          resolve(task);
-        },
-        error => {
-          reject(error);
-        }
-      );
-    });
-  }
-
-  public updateTasks(): Promise<boolean> {
-    return new Promise ((resolve, reject) => {
-      this.getTasks().subscribe(
+      this._commonProvider.performRequest('tasks/', 'GET').subscribe(
         (data: any) => {
           const jsonConvert: JsonConvert = new JsonConvert();
           const tasks: Array<Task> = jsonConvert.deserializeArray(data.results, Task);
           for (const task of tasks) {
-            this.setCategory(task);
+            this._setCategory(task);
           }
           this._tasks.next(tasks);
           resolve();
@@ -90,13 +48,53 @@ export class TasksProvider {
     });
   }
 
-  private getTasks() {
-    return this.commonProvider.performRequest('tasks/', 'GET');
+  public createTask(data: any) {
+    return new Promise ((resolve, reject) => {
+      data.start_date = new Date(data.startDate);
+      data.end_date = new Date(data.endDate);
+      this._commonProvider.performRequest('tasks/', 'POST', data).subscribe(
+        (data: any) => {
+          const jsonConvert: JsonConvert = new JsonConvert();
+          const task: Task = jsonConvert.deserialize(data, Task);
+          const tasks: Array<Task> = this._tasks.getValue();
+          this._setCategory(task);
+          tasks.push(task);
+          resolve(task);
+        },
+        error => {
+          reject(error);
+        }
+      );
+    });
   }
 
-  private setCategory(task) {
-    task.category = this.categoriesProvider.categories.getValue().filter(
-      category => category.url === task.categoryUrl
-    )[0];
+  public updateTask(url: string, data: any) {
+    return new Promise ((resolve, reject) => {
+      data.start_date = new Date(data.startDate);
+      data.end_date = new Date(data.endDate);
+      this._commonProvider.performRequest(url, 'PUT', data).subscribe(
+        (data: any) => {
+          const jsonConvert: JsonConvert = new JsonConvert();
+          const task: Task = jsonConvert.deserialize(data, Task);
+          const tasks: Array<Task> = this._tasks.getValue();
+          const index = tasks.findIndex(x => x.url === url);
+
+          this._setCategory(task);
+          tasks[index] = Object.assign(tasks[index], task);
+
+          resolve(task);
+        },
+        error => {
+          reject(error);
+        }
+      );
+    });
   }
+
+  private _setCategory(task) {
+    task.category = this._categoriesProvider.categories.getValue().find(
+      category => category.url === task.categoryUrl
+    );
+  }
+
 }
