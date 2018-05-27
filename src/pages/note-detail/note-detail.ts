9cba020aa24749f834d97ac61d1a6391e0782786
Hristo Gatsinski <gatsinski@gmail.com>
2018-05-27 11:41:00 +0300

    Change `NoteDetailPage`

--- src/pages/note-detail/note-detail.ts
+++ src/pages/note-detail/note-detail.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
 import { NavController, NavParams } from 'ionic-angular';
 
 import { Note } from '../../models/note';
@@ -11,16 +12,34 @@ import { NoteUpdatePage } from '../note-update/note-update';
 })
 export class NoteDetailPage {
   public note: Note;
+  public translationParams = {
+    createdAt: {},
+    updatedAt: {}
+  };
 
   constructor(
+    private _datePipe: DatePipe,
     private _navCtrl: NavController,
     private _navParams: NavParams
   ) {
     this.note = this._navParams.get('note');
+
+    this.translationParams.createdAt = this._formatDate(this.note.createdAt);
+    this.translationParams.updatedAt = this._formatDate(this.note.updatedAt);
   }
 
   public navigateToUpdatePage() {
     this._navCtrl.push(NoteUpdatePage, { note: this.note });
+  }
+
+  private _formatDate(date: Date): object {
+    const dateFormat = 'dd.MM';
+    const timeFormat = 'HH:mm';
+
+    return {
+      date: this._datePipe.transform(date, dateFormat),
+      time: this._datePipe.transform(date, timeFormat),
+    };
   }
 
 }
