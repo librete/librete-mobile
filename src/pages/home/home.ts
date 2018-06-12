45db87fb009547dc525f9621eb094dbf2a139590
Hristo Gatsinski <gatsinski@gmail.com>
2018-06-12 21:38:30 +0300

    Read events, notes and tasks in `HomePage` and add stats

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
 import { SettingsProvider } from '../../providers/settings/settings';
+import { EventsProvider } from '../../providers/events/events';
+import { NotesProvider } from '../../providers/notes/notes';
+import { TasksProvider } from '../../providers/tasks/tasks';
 import { CategoriesProvider } from '../../providers/categories/categories';
 
 @Component({
@@ -10,10 +13,14 @@ import { CategoriesProvider } from '../../providers/categories/categories';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public stats: object;
 
   constructor(
     private _translateService: TranslateService,
     private _settingsProvider: SettingsProvider,
+    private _eventsProvider: EventsProvider,
+    private _notesProvider: NotesProvider,
+    private _tasksProvider: TasksProvider,
     private _categoriesProvider: CategoriesProvider
   ) {
 
@@ -25,7 +32,25 @@ export class HomePage {
       }
     );
 
-    _categoriesProvider.readCategories();
+    this._categoriesProvider.readCategories().then(
+      data => {
+        Promise.all([
+          this._eventsProvider.readEvents(),
+          this._notesProvider.readNotes(),
+          this._tasksProvider.readTasks(),
+        ]).then(
+          value => this._addStats()
+        );
+      }
+    );
+  }
+
+  private _addStats() {
+    this.stats = {
+      'events': this._eventsProvider.events.getValue().length,
+      'notes': this._notesProvider.notes.getValue().length,
+      'tasks': this._tasksProvider.tasks.getValue().length
+    };
   }
 
 }
